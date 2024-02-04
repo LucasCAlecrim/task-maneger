d8a0faff18a4a6af77177919f8560ef5380c2800
LucasCAlecrim <lucascardosoalecrim99@gmail.com>
2024-02-04 15:13:56 -0300

    Criação do projeto Task Maneger

---
+++ src/test/java/com/task/maneger/TaskManegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.maneger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
