5c8bffee65daf7c1f2f180ed993bb71077d937da
LucasCAlecrim <lucascardosoalecrim99@gmail.com>
2024-02-25 19:23:48 -0300

    Autenticação de Usuario no Model

---
+++ src/main/java/com/task/maneger/model/UsurioAutenticado.java
@@ -0,0 +1,12 @@
+package com.task.maneger.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsurioAutenticado {
+    
+    private String username;
+    private String password;
+}
